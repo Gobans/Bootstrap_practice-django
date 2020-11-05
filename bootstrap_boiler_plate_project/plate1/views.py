4dc57741c4b34cc6b25f0e3873f81466be6e023f
Gobans <audghks4452@gmail.com>
2020-11-05 22:42:53 +0900

    complate

--- bootstrap_boiler_plate_project/plate1/views.py
+++ bootstrap_boiler_plate_project/plate1/views.py
@@ -2,17 +2,15 @@ from django.shortcuts import render,redirect
 from .forms import AskForm 
 from .models import Ask
 
-# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         post_form = AskForm(request.POST)
         
-        #모델 폼이 유효 할 경우
         if post_form.is_valid():
             post_form.save()
             return redirect('posts:list')
     
-    #Get 방식일 경우
     else:
         post_form = AskForm()
 
